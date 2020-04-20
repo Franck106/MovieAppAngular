4dcd5db400e5826d90ba7a4fb1b4ba2f29286d21
Franck <franssois78@yahoo.fr>
2020-04-20 11:40:30 +0200

    account under construction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { Network } from "@ngx-pwa/offline";
 import { Observable } from "rxjs";
 import { AuthServiceService } from "./core/auth/auth-service.service";
+import { StoreService } from "./core/store/store.service";
+import { ReservationsService } from "./core/reservations/reservations.service";
 
 @Component({
   selector: "app-root",
@@ -17,9 +19,19 @@ import { AuthServiceService } from "./core/auth/auth-service.service";
     </div>
   `,
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   isAuthenticated$: Observable<boolean>;
   reservationsCount$: Observable<number>;
 
-  constructor(protected network: Network, private auth: AuthServiceService) {}
+  constructor(
+    protected network: Network,
+    private auth: AuthServiceService,
+    private store: StoreService,
+    private reservations: ReservationsService
+  ) {}
+
+  ngOnInit(): void {
+    this.isAuthenticated$ = this.auth.isAuthenticated;
+    this.reservationsCount$ = this.store.select("reservationsCount");
+  }
 }
