0adec6d8320b7df970fa867c82a6c2d956b646ae
Franck106 <francois612-resa@yahoo.fr>
2020-04-22 19:04:53 +0200

    Slideshow display

--- src/app/features/cinema/cinema-routing.module.ts
+++ src/app/features/cinema/cinema-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 
-import { MoviesPages } from "./pages/movies/movies.pages";
+import { MoviesPage } from "./pages/movies/movies.page";
 import { MoviePage } from "./pages/movie/movie.page";
 import { TheatersPage } from "./pages/theaters/theaters.page";
 import { TheaterPage } from "./pages/theater/theater.page";
@@ -9,7 +9,7 @@ import { TheaterPage } from "./pages/theater/theater.page";
 const routes: Routes = [
   { path: "movie/:id", component: MoviePage },
   { path: "theater/:id", component: TheaterPage },
-  { path: "movies", component: MoviesPages },
+  { path: "movies", component: MoviesPage },
   { path: "theaters", component: TheatersPage },
 ];
 
